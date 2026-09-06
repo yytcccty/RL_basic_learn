f33bb638066d142c2f90c1a45ec080b0f6ab3e90
Cris <yytcctyy@outlook.com>
2026-09-06 18:41:07 +0800

    Achieved n-step Sarsa

--- Sarsa.py
+++ Sarsa.py
@@ -45,12 +45,17 @@ class CliffWalkingEnv:
 
 
 class Sarsa:
-    def __init__(self, alpha, gamma, eps, ncol, nrow, n_act=4):
+    def __init__(self, n_steps, alpha, gamma, eps, ncol, nrow, n_act=4):
         self.alpha = alpha
         self.gamma = gamma
         self.eps = eps
         self.n_act = n_act
         self.Q = np.zeros((nrow * ncol, n_act))
+        self.n_step = n_step
+
+        self.r_list = []
+        self.a_list = []
+        self.s_list = []
         self.rng = np.random.default_rng(seed=41)
 
     def perform_action(self, s_cur):
@@ -63,16 +68,36 @@ class Sarsa:
         else:
             return np.argmax(self.Q[s_cur])
 
-    def update(self, s_cur, a_cur, r, s_next, a_next):
+    def update(self, s_cur, a_cur, r, s_next, a_next, done):
         """
         :param s_cur: Current state
         :param a_cur: Current action
         :param r: Reward r=f(s_cur, a_cur, s_next)
         :param s_next: The next state
         :param a_next: The next action
-        Update Q_sa using Temporal Difference Formulation
+        :param done: Whether the episode has ended
+        Update Q_sa using n-step Temporal Difference Formulation
         """
-        self.Q[s_cur][a_cur] += self.alpha * (r + self.gamma * self.Q[s_next][a_next] - self.Q[s_cur][a_cur])
+        self.s_list.append(s_cur)
+        self.r_list.append(r)
+        self.a_list.append(a_cur)
+        if len(self.s_list) == self.n_step:
+            tmp_G = self.Q[s_next, a_next]
+            for i in reversed(range(self.n_step)):
+                tmp_G = self.gamma * tmp_G + self.r_list[i]
+                if done and i > 0:
+                    s = self.s_list[i]
+                    a = self.a_list[i]
+                    self.Q[s, a] += self.alpha * (tmp_G - self.Q[s, a])
+            s = self.s_list.pop(0)
+            a = self.a_list.pop(0)
+            self.r_list.pop(0)
+            self.Q[s, a] += self.alpha * (tmp_G - self.Q[s, a])
+
+        if done:
+            self.s_list = []
+            self.r_list = []
+            self.a_list = []
 
 
 def printPolicy(agent, env, disaster=None, end=None):
@@ -103,21 +128,22 @@ if __name__ == '__main__':
     gamma = 0.9
     eps = 0.1
     episodes = 500
+    n_step = 5
     """
     Coding
     """
     env = CliffWalkingEnv(ncol, nrow)
-    agent = Sarsa(alpha, gamma, eps, ncol, nrow)
+    agent = Sarsa(n_step, alpha, gamma, eps, ncol, nrow)
 
     retn_list = []
-    for cnt in tqdm(range(episodes), desc='Episode'):
+    for cnt in tqdm(range(episodes), desc=f'Episodes'):
         s_cur = env.reset()
         a_cur = agent.perform_action(s_cur)
         tmp_retn = 0.
         while True:
             (s_next, reward, done) = env.interact(a_cur)
             a_next = agent.perform_action(s_next)
-            agent.update(s_cur, a_cur, reward, s_next, a_next)
+            agent.update(s_cur, a_cur, reward, s_next, a_next, done)
             tmp_retn += reward  # No discount here
             s_cur = s_next
             a_cur = a_next
