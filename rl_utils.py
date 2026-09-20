0d127af4b24631400587b5abfb169a81fa1208f7
yytcccty <26032379r@polyu.edu.hk>
2026-09-21 00:59:03 +0800

    Fix train_off_policy function

--- rl_utils.py
+++ rl_utils.py
@@ -65,11 +65,12 @@ def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size
         with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
             for i_episode in range(int(num_episodes / 10)):
                 episode_return = 0
-                state = env.reset()
+                state, _ = env.reset()
                 done = False
                 while not done:
                     action = agent.take_action(state)
-                    next_state, reward, done, _ = env.step(action)
+                    next_state, reward, terminated, truncated, info = env.step(action)
+                    done = terminated or truncated
                     replay_buffer.add(state, action, reward, next_state, done)
                     state = next_state
                     episode_return += reward
