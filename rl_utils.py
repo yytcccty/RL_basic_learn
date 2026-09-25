9a84d4fde19f4d2fbe7234597e87520a044333f6
Cris <yytcctyy@outlook.com>
2026-09-25 18:07:50 +0800

    Basic improve

--- rl_utils.py
+++ rl_utils.py
@@ -95,4 +95,4 @@ def compute_advantage(gamma, lmbda, td_delta):
         advantage = gamma * lmbda * advantage + delta
         advantage_list.append(advantage)
     advantage_list.reverse()
-    return torch.tensor(advantage_list, dtype=torch.float)
+    return torch.tensor(np.array(advantage_list), dtype=torch.float)
